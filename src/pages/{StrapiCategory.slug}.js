e5f37af696b7a83ed1fade46339bab098ac952b6
wnhollington <wnhollington@gmail.com>
2022-03-03 14:41:03 -0700

    add articles to category page

--- src/pages/{StrapiCategory.slug}.js
+++ src/pages/{StrapiCategory.slug}.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { Link } from 'gatsby'
 
 // Components
 import Layout from '../components/layout'
@@ -7,15 +8,23 @@ import Layout from '../components/layout'
 // Render
 export default function Category(props) {
   const title = props.data.strapiCategory.name
+  const articles = props.data.strapiCategory.articles
+
   return (
     <Layout pageTitle="Categories"> 
       <h1>{title}</h1>
-      <p>General Category Content</p>
-      <ul>
-        <li>Post 1</li>
-        <li>Post 2</li>
-        <li>Post 3</li>
-      </ul>
+      {articles.map(article => {
+        const title = article.title
+        const slug = article.slug
+        const description = article.description
+        return (
+          <article class="blog-post">
+            <h2 class="blog-post-title">{title}</h2>
+            <p>{description}</p>
+            <Link to={`/${slug}`}>Continue Reading</Link>
+          </article>
+        )
+      })}
     </Layout>
   )
 }
@@ -25,6 +34,13 @@ export const query = graphql`
   query Category($slug: String) {
     strapiCategory(slug: {eq: $slug}) {
       name
+      articles {
+        title
+        author
+        created_at
+        slug
+        description
+      }
     }
   }
 `
