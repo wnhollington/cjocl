e1b146a4e0cf82e1102fa2ffe8dff92ff3e85dca
wnhollington <wnhollington@gmail.com>
2022-03-04 06:49:02 -0700

    add basic blog layout

--- src/templates/article.js
+++ src/templates/article.js
@@ -5,33 +5,58 @@ import { Link } from 'gatsby'
 
 // Components
 import Layout from '../components/layout'
+import Sidebar from '../components/sidebar'
 
 // Render
 const Article = ({ data, pageContext }) => {
   const { previous, next } = pageContext
   const title = data.strapiArticle.title
   const content = data.strapiArticle.content
+  const date = data.strapiArticle.created_at
+  const author = data.strapiArticle.author.name
 
   return (
-    <Layout pageTitle="Page">
-      
-      {/* Article Content */}
-      <h1>{title}</h1>
-      <ReactMarkdown children={content} />
+    <Layout pageTitle={title}>
 
-      {/* Pagination */}
-      <nav class="blog-pagination" aria-label="Pagination">
-        {previous && (
-          <Link to={`/${previous.slug}`} rel="previous">
-              <h4><span></span>{previous.title}</h4>
-          </Link>
-        )}
-        {next && (
-          <Link to={`/${next.slug}`} rel="next">
-            <h4>{next.title}<span></span></h4>
-          </Link>
-        )}
-      </nav>
+      <div className="row g-5">
+
+        <div className="col-md-8">
+          {/* Article */}
+          <article>
+            {/* Header */}
+            <header className='mb-4'>
+              <h1 className="fw-bolder mb-1">{title}</h1>
+              <p className="text-muted fst-italic mb-2">Post on {date} by {author}</p>
+              <Link to="/" className="badge bg-secondary text-decoration link-link">Tech</Link>
+            </header>
+
+            {/* Feature Image */}
+            <figure className='mb-4'>
+              <img src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="feature image" className="img-fluid rounded" />
+            </figure>
+
+            {/* Article Content */}
+            <ReactMarkdown children={content} className="mb-5"/>
+          </article>
+
+          {/* Pagination */}
+          <nav class="blog-pagination" aria-label="Pagination">
+            {previous && (
+              <Link to={`/${previous.slug}`} rel="previous">
+                  <h4><span></span>{previous.title}</h4>
+              </Link>
+            )}
+            {next && (
+              <Link to={`/${next.slug}`} rel="next">
+                <h4>{next.title}<span></span></h4>
+              </Link>
+            )}
+          </nav>
+        </div>
+
+        {/* Sidebar */}
+        <Sidebar />
+      </div>
       
     </Layout>
   )
@@ -46,6 +71,9 @@ export const query = graphql`
       created_at
       content
       slug
+      author {
+        name
+      }
     }
   }
 `
