7e5439743f0f48a55ff613276bc07bc66b2ac6c4
wnhollington <wnhollington@gmail.com>
2022-03-04 07:34:39 -0700

    add feature image to blog posts

--- src/templates/article.js
+++ src/templates/article.js
@@ -2,37 +2,39 @@ import * as React from 'react'
 import { graphql } from 'gatsby'
 import ReactMarkdown from "react-markdown"
 import { Link } from 'gatsby'
+import { GatsbyImage } from 'gatsby-plugin-image'
 
 // Components
 import Layout from '../components/layout'
-import Sidebar from '../components/sidebar'
 
 // Render
 const Article = ({ data, pageContext }) => {
   const { previous, next } = pageContext
   const title = data.strapiArticle.title
   const content = data.strapiArticle.content
+  const img = data.strapiArticle.image.localFile.childImageSharp.gatsbyImageData
   const date = data.strapiArticle.created_at
-  const author = data.strapiArticle.author.name
+  const author = data.strapiArticle.author
+  const category = data.strapiArticle.category
 
   return (
     <Layout pageTitle={title}>
+
+      {/* Header */}
+      <header className='mb-4'>
+        <h1 className="fw-bolder mb-1">{title}</h1>
+        <p className="text-muted fst-italic mb-2">Post on {date} by {author.name}</p>
+        <Link to={`/${category.slug}`} className="badge bg-secondary text-decoration link-link">{category.name}</Link>
+      </header>
 
       <div className="row g-5">
 
         <div className="col-md-8">
           {/* Article */}
           <article>
-            {/* Header */}
-            <header className='mb-4'>
-              <h1 className="fw-bolder mb-1">{title}</h1>
-              <p className="text-muted fst-italic mb-2">Post on {date} by {author}</p>
-              <Link to="/" className="badge bg-secondary text-decoration link-link">Tech</Link>
-            </header>
-
             {/* Feature Image */}
             <figure className='mb-4'>
-              <img src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="feature image" className="img-fluid rounded" />
+              <GatsbyImage image={img} alt={title} className="img-fluid rounded" />
             </figure>
 
             {/* Article Content */}
@@ -55,7 +57,18 @@ const Article = ({ data, pageContext }) => {
         </div>
 
         {/* Sidebar */}
-        <Sidebar />
+        <aside className='col-md-4 mt-15'>
+          <div className="p-4 mb-3 bg-light rounded text-center">
+            <GatsbyImage
+              image={author.picture.localFile.childImageSharp.gatsbyImageData}
+              alt={title}
+              className="rounded"
+            />
+            <h4 className='fst-italic'>{author.name}</h4>
+            <p>{author.email}</p>
+            <p className="mb-0">General description of author</p>
+          </div>
+        </aside>
       </div>
       
     </Layout>
@@ -70,9 +83,28 @@ export const query = graphql`
       title
       created_at
       content
+      image {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
       slug
       author {
         name
+        email
+        picture {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(width: 250)
+            }
+          }
+        }
+      }
+      category {
+        name
+        slug
       }
     }
   }
