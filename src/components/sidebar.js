5b7b522c1cb8af0fcb0adf276dc64f488526fe01
wnhollington <wnhollington@gmail.com>
2022-03-05 14:03:09 -0700

    add category list to sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,12 +1,17 @@
 import * as React from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faLinkedin,
   faTwitter
 } from '@fortawesome/free-brands-svg-icons'
 
+// Hooks
+import { useSiteCategories } from "../hooks/use-site-categories"
+
 const Sidebar = () => {
+  const categories = useSiteCategories();
+
   return (
 
     <div class="col-md-4">
@@ -20,18 +25,9 @@ const Sidebar = () => {
         <div class="p-4">
           <h4 class="fst-italic">Archives</h4>
           <ol class="list-unstyled mb-0">
-            <li><a href="">March 2021</a></li>
-            <li><a href="">February 2021</a></li>
-            <li><a href="">January 2021</a></li>
-            <li><a href="">December 2020</a></li>
-            <li><a href="">November 2020</a></li>
-            <li><a href="">October 2020</a></li>
-            <li><a href="">September 2020</a></li>
-            <li><a href="">August 2020</a></li>
-            <li><a href="">July 2020</a></li>
-            <li><a href="">June 2020</a></li>
-            <li><a href="">May 2020</a></li>
-            <li><a href="">April 2020</a></li>
+            {categories.map(({ node }) => {
+              return <li className='mt-1'><Link to={`/${node.slug}`}>{node.name}</Link></li>
+            })}
           </ol>
         </div>
 
