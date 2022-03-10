589638109bb557993c1e631bc53708cb71ce968e
wnhollington <wnhollington@gmail.com>
2022-03-10 07:27:33 -0700

    add email input required

--- src/components/signup/mailchimpForm.js
+++ src/components/signup/mailchimpForm.js
@@ -32,7 +32,7 @@ export default class MailChimpForm extends React.Component {
         <div id="submit-message" class="alert my-3 py-2"></div>
         <form onSubmit={this._handleSubmit} id="form">
           <div class="form-group mb-4">
-            <input type="email" label="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address" onChange={this.handleChange} />
+            <input type="email" label="email" name="email" id="email" class="form-control input-sm" placeholder="Email Address" onChange={this.handleChange} required/>
           </div>
 
           <input type="submit" label="submit" id="submit" value="Signup" class="btn btn-primary btn-block" />
