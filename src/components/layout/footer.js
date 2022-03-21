dc695bb23e56d88ef3accc38b2af238032d06dce
wnhollington <wnhollington@gmail.com>
2022-03-20 18:42:36 -0600

    add smooth scroll to .toTop

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -16,7 +16,12 @@ const Footer = () => {
       <div className="nav-scroller py-1 mb-2">
         <Nav edges={pages}/>
       </div>
-      <Link to="#" class="to-top btn btn-primary"><FontAwesomeIcon icon={faUpLong} size="1x" /></Link>
+      <Link to="#" class="to-top btn btn-primary">
+        <FontAwesomeIcon 
+          icon={faUpLong} size="1x"
+          onClick={window.scroll({top: 0, left: 0, behavior: 'smooth'})}
+        />
+      </Link>
     </footer>
   )
 }
